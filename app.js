52ee67414ead8c35db4b91918ccacccc748c56e3
SILVIO SOUZA <sjblu7@gmail.com>
2021-03-03 20:47:53 -0400

    Add Query Params e Route Params

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server Running!!')
+listaUsuarios = [
+    'Silvio',
+    'Nykolle',
+    'Ivens',
+    'Nicolas',
+    'Natalia',
+    'Jailson'
+]
+
+
+// Query Params
+// ex.: localhost:3000/usuarios?nome=Silvio
+app.get('/usuarios/', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i === nome)
+    return res.json(listaRetorno)
 })
 
-app.get('/', (req, res) => {
-    return res.send('Hello World')
+
+// Route Params
+app.get('/:usuario', (req, res) => {
+    const { usuario } = req.params;
+    return res.send(`Hello World ${usuario}`)
+    // return res.send(`Hello World ${req.params.usuario}`)
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server Running!!')
 })
